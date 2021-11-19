ea4e26ca767130949bf6456beb9e8d27eaf18466
Juno Nguyen <juno.ngx@gmail.com>
2021-11-19 18:56:20 +0800

    Add line breaks between functions for splashState

--- states/splashState.lua
+++ states/splashState.lua
@@ -1,10 +1,12 @@
 splashState = {
 	name = "splash",
 	splashTimer,
+
 	init = function(self)
 		fadeOverlay:fade("in", 30)
 		self.splashTimer = 45
 	end,
+
 	update = function(self)
 		if (self.splashTimer > 0) then
 			self.splashTimer -= 1
@@ -12,6 +14,7 @@ splashState = {
 			transit(menuState)
 		end
 	end,
+	
 	draw = function()
 		spr(192, 32, 48, 64, 32)
 	end
